c0a06fe00b854189b0ce22db196a89b1c9ece2df
AvhiMaz <avhimazumder5@outlook.com>
2025-03-28 20:30:39 +0530

    init contract
    Signed-off-by: AvhiMaz <avhimazumder5@outlook.com>

---
+++ contract/programs/contract/src/lib.rs
@@ -0,0 +1,16 @@
+use anchor_lang::prelude::*;
+
+declare_id!("5Fy9pnLmcQu9vsM6o3Vwc2tzytBxt4MGL5SBGboqyBJ");
+
+#[program]
+pub mod contract {
+    use super::*;
+
+    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+        msg!("Greetings from: {:?}", ctx.program_id);
+        Ok(())
+    }
+}
+
+#[derive(Accounts)]
+pub struct Initialize {}
