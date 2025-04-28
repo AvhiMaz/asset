de2cdee642299663cc92745116965bad1815db96
AvhiMaz <avhimazumder5@outlook.com>
2025-04-28 13:23:39 +0530

    feat: add frontend
    Signed-off-by: AvhiMaz <avhimazumder5@outlook.com>

---
+++ frontend/app/layout.tsx
@@ -0,0 +1,23 @@
+import type React from "react";
+import "@/app/globals.css";
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata = {
+  title: "Timeloop Vault | Lock Assets in Time",
+  description:
+    "Lock digital assets on Solana and schedule their delivery to a recipient at a future date.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
---
+++ frontend/components/bg.tsx
@@ -0,0 +1,46 @@
+
+"use client";
+
+import { motion } from "framer-motion";
+import React from "react";
+import { AuroraBackground } from "../components/ui/aurora-background";
+import { ArrowRight } from "lucide-react";
+import { Button } from "./ui/button";
+
+export function Background() {
+  return (
+    <AuroraBackground>
+      <motion.div
+        initial={{ opacity: 0.0, y: 40 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{
+          delay: 0.3,
+          duration: 0.8,
+          ease: "easeInOut",
+        }}
+        className="relative min-h-screen w-full flex flex-col gap-4 items-center justify-center  lowercase"
+      >
+        <div className="text-4xl md:text-6xl dark:text-white text-center lowercase tracking-tighter">
+          Lock asset in time, Deliver to the future
+        </div>
+        <div className="font-extralight text-md mx-10 lg:text-xl text-center tracking-tighter">
+          Timeloop Vault lets you lock any digital asset on Solana and schedule its delivery to a recipient at a future date.
+        </div>
+        <div className="font-extralight text-md mx-10 lg:text-xl text-center tracking-tighter">
+          lock a variety of assets, including:
+        </div>
+
+        <div className="flex gap-3 mt-6">
+          <Button className="lowercase">
+            Create Vault <ArrowRight className="w-[1px] h-[1px]"/>
+          </Button>
+          <Button className="lowercase"> 
+            Learn More
+          </Button>
+        </div>
+
+      </motion.div>
+    </AuroraBackground>
+  );
+}
+
