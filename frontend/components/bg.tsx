ccde656ab6b9315d7ba1c5e34943c4906dbb2e39
AvhiMaz <avhimazumder5@outlook.com>
2025-04-28 19:14:04 +0530

    fix wallet padding
    Signed-off-by: AvhiMaz <avhimazumder5@outlook.com>

--- frontend/components/bg.tsx
+++ frontend/components/bg.tsx
@@ -20,12 +20,12 @@ export function Background() {
 
   return (
     <AuroraBackground>
-      <nav className="py-4 fixed top-0 md:top-10 lg:top-10 left-0 right-0 lg:left-50 lg:right-50 rounded-lg backdrop-blur-xs z-50 border">
+      <nav className="fixed top-10 md:top-10 lg:top-10 left-0 right-0 lg:left-50 lg:right-50 rounded-lg backdrop-blur-xs z-50 border">
         <div className="container mx-auto flex items-center justify-between px-4">
           <div className="flex items-center gap-2">
             <Link href="/" className="font-medium text-xl tracking-tighter">timeloop vault</Link>
           </div>
-          <div className="flex items-center gap-4">
+          <div className="flex items-center ">
             <div
               className="border-zinc-300 text-xl font-medium flex items-center tracking-tighter text-center cursor-pointer"
             >
@@ -33,8 +33,8 @@ export function Background() {
                 background: "transparent",
                 color: "#111",
                 fontSize: "20px",
-                padding: "6px 2px",
                 fontWeight: "500",
+                padding: "0",
                 textTransform: "lowercase",
               }} />
             </div>
